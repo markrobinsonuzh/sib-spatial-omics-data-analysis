2e25f6ddb22bc653b6c26cfadf19682ad57844d3
mjemons <martin.emons@gmail.com>
2024-06-17 17:04:07 +0200

    took out unused functions

--- utils.R
+++ utils.R
@@ -44,63 +44,6 @@ suppressPackageStartupMessages({
   ppp(xy[, 1], xy[, 2], window = w, marks = factor(m))
 }
 
-
-#' Compute the relative risk of a point pattern
-#'
-#' @param pp a point pattern to compute
-#' @param alpha significance level alpha
-#'
-#' @return a list with the relative risk results
-#' @export
-#'
-#' @examples
-pValuesHotspotMarks <- function(pp, alpha = 0.05){
-  # Code source: https://idblr.rbind.io/post/pvalues-spatial-segregation/
-  
-  # Significant p-values assumming normality of the Poisson process
-  ## relrisk() computes standard errors based on asymptotic theory, assuming a Poisson process
-  # call relative risk function
-  f1 <- relrisk(pp_sel,se=TRUE)
-  
-  z <- qnorm(alpha/2, lower.tail = F)     # z-statistic
-  f1$u <- f1$estimate + z*f1$SE           # Upper CIs
-  f1$l <- f1$estimate - z*f1$SE           # Lower CIs
-  mu_0 <- as.vector(table(spatstat.geom::marks(pp))/pp$n) # null expectations by type
-  f1$p <- f1$estimate # copy structure of pixels, replace values
-  for (i in 1:length(f1$p)) {
-    f1$p[[i]]$v <- factor(ifelse(mu_0[i] > f1$u[[i]]$v, "lower",
-                                ifelse( mu_0[i] < f1$l[[i]]$v, "higher", "none")),
-                          levels = c("lower", "none", "higher"))
-  }
-  return(f1)
-}
-
-#' Compute p-values of the hotspots
-#'
-#' @param pp a point pattern to compute
-#' @param alpha significance level alpha
-#'
-#' @return a density estimate
-#' @export
-#'
-#' @examples
-pValuesHotspot <- function(pp, alpha = 0.05){
-  # Code source: https://idblr.rbind.io/post/pvalues-spatial-segregation/
-  # density estimate for all marks
-  f1 <- density(unmark(pp), se = TRUE)
-  # Significant p-values assumming normality of the Poisson process
-  z <- qnorm(alpha/2, lower.tail = F)     # z-statistic
-  f1$u <- f1$estimate + z*f1$SE           # Upper CIs
-  f1$l <- f1$estimate - z*f1$SE           # Lower CIs
-  mu_0 <- intensity(unmark(pp)) # null expectations by type
-  f1$p <- f1$estimate # copy structure of pixels, replace values
-  f1$p$v <- factor(ifelse(mu_0 > f1$u$v, "lower",
-                          ifelse( mu_0 < f1$l$v, "higher", "none")),
-                   levels = c("lower", "none", "higher"))
-  
-  return(f1)
-}
-
 #' Calculate a spatstat metric
 #'
 #' @param fun which function to calculate
